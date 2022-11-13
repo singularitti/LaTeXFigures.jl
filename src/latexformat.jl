dcd3557ab36a049979db09db5e7a8ae58ded85df
singularitti <singularitti@outlook.com>
2022-11-12 23:07:58 -0500

    Fix docs escape for `latexformat`

--- src/latexformat.jl
+++ src/latexformat.jl
@@ -1,9 +1,9 @@
 export latexformat
 
 """
-    latexformat(figure::Figure; indent=' '^4, newline='\n')
-    latexformat(figure::Subfigure; indent=' '^4, newline='\n')
-    latexformat(figure::TwoSubfigures; indent=' '^4, newline='\n')
+    latexformat(figure::Figure; indent=' '^4, newline='\\n')
+    latexformat(figure::Subfigure; indent=' '^4, newline='\\n')
+    latexformat(figure::TwoSubfigures; indent=' '^4, newline='\\n')
 
 Return a formatted LaTeX string representing the `figure`.
 """
